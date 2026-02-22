991f2e4b0e26b148dc2928b2c00ef8d961910d11
cleitonandrade1500 <cleitonandrade1500@gmail.com>
2026-02-22 20:07:49 -0300

    Atualizar o main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,40 +1,28 @@
 #include "natives.h"
 
-// Definições para o compilador Clang no PS4
 typedef unsigned long size_t;
 #define NULL 0
 
-// EXPORTAÇÃO OBRIGATÓRIA PARA GOLDHEN/FPKG
 extern "C" {
     __attribute__((visibility("default"))) int module_start(size_t argc, const void* args);
     __attribute__((visibility("default"))) int module_stop(size_t argc, const void* args);
 }
 
-// Variáveis de Controle
 Ped meuGuarda = 0;
 int tempoGuarda = 0;
-
-void Aguardar(int ms) { WAIT(ms); }
+bool assaltoAtivo = false;
+Vector3 localCrime = {0.0f, 0.0f, 0.0f, 0.0f};
 
 void ProcessarAssalto() {
     Player p = PLAYER::PLAYER_ID(); 
     Ped pP = PLAYER::PLAYER_PED_ID(); 
     Vector3 pos = ENTITY::GET_ENTITY_COORDS(pP, true);
 
-    // TESTE DE INJEÇÃO: Vida regenera instantaneamente (God Mode)
-    ENTITY::SET_ENTITY_HEALTH(pP, 200, 0);
-
-    // SISTEMA DE MÁSCARA E POLÍCIA (Anti-Detecção)
-    if (PED::IS_PED_WEARING_ANY_MASK(pP)) {
-        PLAYER::SET_PLAYER_IGNORE_LOW_HONOR_COMPLAINTS(p, true);
-        PLAYER::SET_PLAYER_WANTED_LEVEL(p, 0, false);
-    }
-
-    // SUPORTE DA GANGUE: Segurar L1 + D-Pad Cima
+    // 1. SUPORTE MARSTON (L1 + Cima)
     if (PAD::IS_CONTROL_PRESSED(0, 0xF7D06352) && PAD::IS_CONTROL_PRESSED(0, 0x911CB91D)) {
         tempoGuarda++;
-        if (tempoGuarda >= 200) {
-            Hash sel = 0x106561CA; // John Marston
+        if (tempoGuarda >= 150) {
+            Hash sel = 0x106561CA;
             STREAMING::REQUEST_MODEL(sel, false);
             if (STREAMING::HAS_MODEL_LOADED(sel)) {
                 if (ENTITY::DOES_ENTITY_EXIST(meuGuarda)) ENTITY::DELETE_ENTITY(&meuGuarda);
@@ -45,30 +33,36 @@ void ProcessarAssalto() {
         }
     } else tempoGuarda = 0;
 
-    // ABERTURA DE PORTAS AUTOMÁTICA (Ao Mirar)
-    if (PLAYER::GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(p, NULL)) {
-        Hash portas[] = { 0x7C030E57, 0x5D35D3D1, 0x242D7D7D, 0x4894379D, 0x5395A642 };
-        for(int i = 0; i < 5; i++) {
-            Object d = OBJECT::GET_CLOSEST_OBJECT_OF_TYPE(pos.x, pos.y, pos.z, 35.0f, portas[i], false, false, true);
-            if (ENTITY::DOES_ENTITY_EXIST(d)) {
-                OBJECT::SET_DOOR_STATE(portas[i], pos.x, pos.y, pos.z, 3, 0.0f);
+    // 2. ABERTURA DE PORTAS E ROUBO
+    Entity alvo;
+    if (PLAYER::GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(p, &alvo)) {
+        Hash m = ENTITY::GET_ENTITY_MODEL(alvo);
+        if (m == 0x2C047466 || m == 0x959E7979) { // Caixas registradoras
+            assaltoAtivo = true; localCrime = pos;
+            PLAYER::SET_PLAYER_WANTED_LEVEL(p, 4, false);
+            Hash portas[] = { 0x7C030E57, 0x5D35D3D1, 0x242D7D7D, 0x4894379D, 0x5395A642 };
+            for(int i = 0; i < 5; i++) {
+                Object d = OBJECT::GET_CLOSEST_OBJECT_OF_TYPE(pos.x, pos.y, pos.z, 50.0f, portas[i], false, false, true);
+                if (ENTITY::DOES_ENTITY_EXIST(d)) OBJECT::SET_DOOR_STATE(portas[i], pos.x, pos.y, pos.z, 3, 0.0f);
             }
+        }
+    }
+
+    // 3. EXPLODIR COFRE E GANHAR DINHEIRO
+    Hash cofres[] = { 0x1873C856, 0x5395A642, 0x0E63B489 };
+    for(int i = 0; i < 3; i++) {
+        Object cf = OBJECT::GET_CLOSEST_OBJECT_OF_TYPE(pos.x, pos.y, pos.z, 3.5f, cofres[i], false, false, true);
+        if (ENTITY::DOES_ENTITY_EXIST(cf) && PAD::IS_CONTROL_JUST_PRESSED(0, 0x07CEABE4)) {
+            FIRE::ADD_EXPLOSION(pos.x, pos.y, pos.z, 2, 1.0f, true, false, 1.0f);
+            ENTITY::DELETE_ENTITY(&cf);
+            CASH::MONEY_ADD_CASH(MISC::GET_RANDOM_INT_IN_RANGE(20000, 400000));
         }
     }
 }
 
-// ENTRY POINT QUE O GOLDHEN ENXERGA
 extern "C" int module_start(size_t argc, const void* args) {
-    // Delay de 45 segundos para estabilizar com o Havana Mod
-    for(int i = 0; i < 450; i++) { 
-        Aguardar(100); 
-    }
-    
-    while (true) {
-        ProcessarAssalto();
-        Aguardar(0); 
-    }
+    for(int i = 0; i < 450; i++) { WAIT(100); } // 45s Delay
+    while (true) { ProcessarAssalto(); WAIT(0); }
     return 0;
 }
-
 extern "C" int module_stop(size_t argc, const void* args) { return 0; }
