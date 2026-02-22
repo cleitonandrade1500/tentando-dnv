a7cdfdcf701f45de068a659f8750bf5f6b777181
cleitonandrade1500 <cleitonandrade1500@gmail.com>
2026-02-22 00:16:06 -0300

    Atualizar o natives.h

--- source/natives.h
+++ source/natives.h
@@ -6,52 +6,55 @@ typedef int Entity;
 typedef int Ped;
 typedef int Object;
 typedef unsigned int Hash;
-
 struct Vector3 { float x, y, z; };
 
 namespace PLAYER {
     extern Player PLAYER_ID();
     extern Ped PLAYER_PED_ID();
     extern bool GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(Player player, Entity* entity);
+    extern void SET_PLAYER_WANTED_LEVEL(Player player, int level, bool p2);
+    extern int GET_PLAYER_WANTED_LEVEL(Player player);
     extern void CLEAR_PLAYER_WANTED_LEVEL(Player player);
 }
-
 namespace ENTITY {
-    extern bool IS_ENTITY_A_PED(Entity entity);
-    extern bool IS_ENTITY_DEAD(Entity entity);
     extern Hash GET_ENTITY_MODEL(Entity entity);
     extern Vector3 GET_ENTITY_COORDS(Entity entity, bool alive);
     extern bool DOES_ENTITY_EXIST(Entity entity);
     extern void SET_ENTITY_AS_MISSION_ENTITY(Entity entity, bool p1, bool p2);
     extern void SET_ENTITY_AS_NO_LONGER_NEEDED(Entity* entity);
+    extern bool IS_ENTITY_A_PED(Entity entity);
+    extern bool IS_ENTITY_DEAD(Entity entity);
 }
-
+namespace PED {
+    extern Ped CREATE_PED(Hash model, float x, float y, float z, float heading, bool p5, bool p6, bool p7, bool p8);
+    extern void SET_PED_RELATIONSHIP_GROUP_HASH(Ped ped, Hash group);
+}
+namespace STREAMING {
+    extern void REQUEST_MODEL(Hash model, bool p1);
+    extern bool HAS_MODEL_LOADED(Hash model);
+}
 namespace TASK {
     extern void TASK_HANDS_UP(Ped ped, int duration, Ped facingPed, int p3, bool p4);
+    extern void TASK_COMBAT_PED(Ped ped, Ped target, int p2, int p3);
 }
-
 namespace OBJECT {
-    extern Object GET_CLOSEST_OBJECT_OF_TYPE(float x, float y, float z, float radius, Hash modelHash, bool isMission, bool p6, bool p7);
-    extern void DOOR_CONTROL(Hash doorHash, float x, float y, float z, bool locked, float xRot, float yRot, float zRot);
+    extern Object GET_CLOSEST_OBJECT_OF_TYPE(float x, float y, float z, float radius, Hash model, bool p5, bool p6, bool p7);
+    extern void DOOR_CONTROL(Hash door, float x, float y, float z, bool locked, float xRot, float yRot, float zRot);
 }
-
-namespace CASH {
-    extern void MONEY_ADD_CASH(int amount);
+namespace HUD {
+    extern void SET_BLIP_SPRITE(int blip, Hash spriteHash);
+    extern void SET_BLIP_COLOUR(int blip, int colour);
 }
-
-namespace MISC {
-    extern Hash GET_HASH_KEY(const char* string);
+namespace MAP {
+    extern int SET_BLIP_FOR_ENTITY(Entity entity);
 }
-
-namespace PAD {
-    extern bool IS_CONTROL_PRESSED(int padIndex, Hash control);
-    extern bool IS_CONTROL_JUST_PRESSED(int padIndex, Hash control);
+namespace CASH { extern void MONEY_ADD_CASH(int amount); }
+namespace MISC { 
+    extern Hash GET_HASH_KEY(const char* str); 
+    extern float GET_DISTANCE_BETWEEN_COORDS(float x1, float y1, float z1, float x2, float y2, float z2, bool useZ);
 }
-
-namespace FIRE {
-    extern void ADD_EXPLOSION(float x, float y, float z, int explosionType, float damageScale, bool isAudible, bool isInvisible, float cameraShake);
-}
+namespace PAD { extern bool IS_CONTROL_PRESSED(int p0, Hash control); extern bool IS_CONTROL_JUST_PRESSED(int p0, Hash control); }
+namespace FIRE { extern void ADD_EXPLOSION(float x, float y, float z, int type, float damage, bool audible, bool invisible, float shake); }
 
 extern "C" void WAIT(int ms);
-
 #endif
