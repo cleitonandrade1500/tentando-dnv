8ef19fcdceb3a23c22a5e446dfb6161edf2e4e15
cleitonandrade1500 <cleitonandrade1500@gmail.com>
2026-02-22 19:33:15 -0300

    Atualizar o natives.h

--- source/natives.h
+++ source/natives.h
@@ -1,69 +1,53 @@
-#include "natives.h"
+#ifndef NATIVES_H
+#define NATIVES_H
 
-typedef unsigned long size_t;
-#define NULL 0
+typedef int Player; 
+typedef int Entity; 
+typedef int Ped; 
+typedef int Object; 
+typedef unsigned int Hash;
 
-extern "C" {
-    __attribute__((visibility("default"))) int module_start(size_t argc, const void* args);
-    __attribute__((visibility("default"))) int module_stop(size_t argc, const void* args);
+struct Vector3 { 
+    float x, y, z; 
+    float padding; 
+};
+
+namespace PLAYER {
+    extern "C" Player PLAYER_ID(); 
+    extern "C" Ped PLAYER_PED_ID(); 
+    extern "C" void SET_PLAYER_WANTED_LEVEL(Player player, int level, bool p2);
+    extern "C" void SET_PLAYER_IGNORE_LOW_HONOR_COMPLAINTS(Player player, bool toggle);
 }
 
-Ped meuGuarda = 0;
-int tempoGuarda = 0;
-
-void Aguardar(int ms) { WAIT(ms); }
-
-void ProcessarAssalto() {
-    Player p = PLAYER::PLAYER_ID(); 
-    Ped pP = PLAYER::PLAYER_PED_ID(); 
-    Vector3 pos = ENTITY::GET_ENTITY_COORDS(pP, true);
-
-    // FIX DA VIDA: Força o HP ao máximo a cada frame (God Mode)
-    ENTITY::SET_ENTITY_HEALTH(pP, 200, 0);
-
-    // FIX DA MÁSCARA E POLÍCIA:
-    // Força o jogo a ignorar crimes mesmo se a polícia estiver perto
-    if (PED::IS_PED_WEARING_ANY_MASK(pP)) {
-        PLAYER::SET_PLAYER_IGNORE_LOW_HONOR_COMPLAINTS(p, true);
-        PLAYER::SET_PLAYER_WANTED_LEVEL(p, 0, false); // Limpa o Wanted se estiver de máscara
-    }
-
-    // SUPORTE DA GANGUE: L1 + Direcional Cima
-    if (PAD::IS_CONTROL_PRESSED(0, 0xF7D06352) && PAD::IS_CONTROL_PRESSED(0, 0x911CB91D)) {
-        tempoGuarda++;
-        if (tempoGuarda >= 150) {
-            Hash sel = 0x106561CA;
-            STREAMING::REQUEST_MODEL(sel, false);
-            if (STREAMING::HAS_MODEL_LOADED(sel)) {
-                if (ENTITY::DOES_ENTITY_EXIST(meuGuarda)) ENTITY::DELETE_ENTITY(&meuGuarda);
-                meuGuarda = PED::CREATE_PED(sel, pos.x + 2.0f, pos.y, pos.z, 0.0f, false, false, false, false);
-                PED::SET_PED_AS_GROUP_MEMBER(meuGuarda, PLAYER::GET_PLAYER_GROUP(p));
-                tempoGuarda = 0;
-            }
-        }
-    } else tempoGuarda = 0;
-
-    // ABERTURA DE PORTAS: Agora com raio maior e forçando "Mission Entity"
-    if (PLAYER::GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(p, NULL)) {
-        Hash portas[] = { 0x7C030E57, 0x5D35D3D1, 0x242D7D7D, 0x4894379D, 0x5395A642 };
-        for(int i = 0; i < 5; i++) {
-            Object d = OBJECT::GET_CLOSEST_OBJECT_OF_TYPE(pos.x, pos.y, pos.z, 50.0f, portas[i], false, false, true);
-            if (ENTITY::DOES_ENTITY_EXIST(d)) {
-                ENTITY::SET_ENTITY_AS_MISSION_ENTITY(d, true, true);
-                OBJECT::SET_DOOR_STATE(portas[i], pos.x, pos.y, pos.z, 3, 0.0f);
-            }
-        }
-    }
+namespace ENTITY {
+    extern "C" Vector3 GET_ENTITY_COORDS(Entity entity, bool alive);
+    extern "C" bool DOES_ENTITY_EXIST(Entity entity); 
+    extern "C" void DELETE_ENTITY(Entity* entity);
+    extern "C" void SET_ENTITY_HEALTH(Entity entity, int health, int p2);
+    extern "C" void SET_ENTITY_AS_MISSION_ENTITY(Entity entity, bool p1, bool p2);
+    extern "C" Hash GET_ENTITY_MODEL(Entity entity);
 }
 
-extern "C" int module_start(size_t argc, const void* args) {
-    // Delay de 45 segundos para o Havana não "matar" o mod
-    for(int i = 0; i < 450; i++) { Aguardar(100); }
-    while (true) {
-        ProcessarAssalto();
-        Aguardar(0); 
-    }
-    return 0;
+namespace PED {
+    extern "C" bool IS_PED_WEARING_ANY_MASK(Ped ped); 
+    extern "C" void SET_PED_AS_GROUP_MEMBER(Ped ped, int groupID);
 }
 
-extern "C" int module_stop(size_t argc, const void* args) { return 0; }
+namespace OBJECT {
+    extern "C" Object GET_CLOSEST_OBJECT_OF_TYPE(float x, float y, float z, float radius, Hash model, bool p5, bool p6, bool p7);
+    extern "C" void SET_DOOR_STATE(Hash doorModel, float x, float y, float z, int state, float angle);
+}
+
+namespace HUD {
+    extern "C" void _DISPLAY_TEXT(const char* text, float x, float y);
+    extern "C" void _SET_TEXT_COLOR(int r, int g, int b, int a);
+    extern "C" void _SET_TEXT_SCALE(float scale, float size);
+}
+
+namespace PAD { 
+    extern "C" bool IS_CONTROL_PRESSED(int p0, Hash control); 
+}
+
+extern "C" void WAIT(int ms);
+
+#endif
