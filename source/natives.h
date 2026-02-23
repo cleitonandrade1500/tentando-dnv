86250a0b0857cb2e8a5a3fece30f86fe12b4393c
cleitonandrade1500 <cleitonandrade1500@gmail.com>
2026-02-22 23:32:28 -0300

    Atualizar o natives.h

--- source/natives.h
+++ source/natives.h
@@ -1,14 +1,8 @@
 #ifndef NATIVES_H
 #define NATIVES_H
 
-// RESOLVE O ERRO: unknown type name 'size_t'
 typedef unsigned long size_t;
-
-typedef int Player; 
-typedef int Entity; 
-typedef int Ped; 
-typedef int Object; 
-typedef unsigned int Hash;
+typedef int Player; typedef int Entity; typedef int Ped; typedef int Object; typedef unsigned int Hash;
 typedef unsigned long uint64_t;
 
 struct Vector3 { float x, y, z; float padding; };
@@ -17,17 +11,19 @@ extern "C" {
     void nativeInit(uint64_t hash);
     void nativePush(uint64_t val);
     uint64_t* nativeCall();
+    void WAIT(int ms);
 }
 
+// Template para funções com retorno (Ex: Player ID)
 template<typename T, typename... Args>
 static T Invoke(uint64_t hash, Args... args) {
     nativeInit(hash);
     uint64_t vars[] = { (uint64_t)args... };
     for (int i = 0; i < sizeof...(args); i++) nativePush(vars[i]);
-    uint64_t* res = nativeCall();
-    return *(T*)res;
+    return *(T*)nativeCall();
 }
 
+// Template para funções sem retorno (Resolve erro de indirection/void)
 template<typename... Args>
 static void InvokeVoid(uint64_t hash, Args... args) {
     nativeInit(hash);
@@ -42,13 +38,6 @@ namespace PLAYER {
     inline void SET_PLAYER_WANTED_LEVEL(Player p, int l, bool p2) { InvokeVoid(0xB7A0914B, p, l, p2); }
 }
 
-namespace ENTITY {
-    inline Vector3 GET_ENTITY_COORDS(Entity e, bool a) { return Invoke<Vector3>(0xA86D3333, e, a); }
-    inline bool DOES_ENTITY_EXIST(Entity e) { return Invoke<bool>(0x367F4B72, e); }
-    inline void DELETE_ENTITY(Entity* e) { InvokeVoid(0x37648392, e); }
-}
-
-// RESOLVE O ERRO: use of undeclared identifier 'STREAMING'
 namespace STREAMING {
     inline void REQUEST_MODEL(Hash m, bool p1) { InvokeVoid(0xFA6A2322, m, p1); }
     inline bool HAS_MODEL_LOADED(Hash m) { return Invoke<bool>(0x32334F22, m); }
@@ -58,5 +47,10 @@ namespace CASH {
     inline void MONEY_ADD_CASH(int amount) { InvokeVoid(0xBC235322, amount); }
 }
 
-extern "C" void WAIT(int ms);
+namespace ENTITY {
+    inline Vector3 GET_ENTITY_COORDS(Entity e, bool a) { return Invoke<Vector3>(0xA86D3333, e, a); }
+    inline bool DOES_ENTITY_EXIST(Entity e) { return Invoke<bool>(0x367F4B72, e); }
+    inline void DELETE_ENTITY(Entity* e) { InvokeVoid(0x37648392, e); }
+}
+
 #endif
